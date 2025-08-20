08640f2bf722ccdfa6b9c827cf6aa88ef605fa73
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-08-20 19:27:13 +0300

    feat: add card wrapper component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,17 +7,18 @@ import {
 } from '@angular/core';
 
 import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
-  @Input( {required: true}) selected!: boolean;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   imagePath() {
